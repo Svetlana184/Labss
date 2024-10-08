b9b6c37ef96c91e5b19772a2131038229068a577
Svetlana184 <sveta.kgd.39@mail.com>
2024-10-08 12:46:03 +0200

    try

---
+++ LAB_2.1/Program.cs
@@ -0,0 +1,12 @@
+﻿try
+{
+    Console.WriteLine("Введите x:");
+    double x = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите y:");
+    double y = double.Parse(Console.ReadLine());
+    Console.WriteLine($"точка с координатами ({x}, {y}), попадает в заданную область - {(x * x + y * y <= 4) && (x * x + y * y >= 1)}");
+}
+catch
+{
+    Console.WriteLine("введите данные правильно");
+}
---
+++ LAB_2.2/Program.cs
@@ -0,0 +1,29 @@
+﻿try
+{
+    Console.WriteLine("Введите трёхзначное число:");
+    int x = int.Parse(Console.ReadLine());
+    if (x / 1000 == 0 && x / 100 != 0)
+    {
+        int sumNum = x / 100 + x % 10 + x % 100 / 10;
+        int sumProizv = (x / 100) * (x % 10) * (x % 100 / 10);
+        if (sumNum / 100 == 0 && sumNum / 10 != 0)
+        {
+            Console.WriteLine("сумма цифр числа - двузначное число");
+        }
+        else { Console.WriteLine("сумма цифр числа - не двузначное число"); }
+
+        if (sumProizv / 1000 == 0 && sumProizv / 100 != 0)
+        {
+            Console.WriteLine("произведение цифр числа - трёхзначное число");
+        }
+        else { Console.WriteLine("произведение цифр числа - не трёхзначное число"); }
+    }
+    else { Console.WriteLine("введите трёхзначное число!"); }
+
+
+}
+
+catch
+{
+    Console.WriteLine("введите данные правильно");
+}

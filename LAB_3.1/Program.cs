3ca7c4c70261c0fc6e33110a391f130b8070fa8b
Svetlana184 <sveta.kgd.39@gmail.com>
2024-10-14 11:04:02 +0200

    try

---
+++ LAB_3.1/Program.cs
@@ -0,0 +1,18 @@
+﻿try
+{
+    int x = 1,  sum = 0;
+    while (x != 0)
+    {
+        Console.WriteLine("введите число");
+        x = int.Parse(Console.ReadLine());
+        if (x>0)
+        {
+            sum += x;
+        }
+    }
+    Console.WriteLine($"сумма положительных чисел последовательности равна {sum}");
+}
+catch
+{
+    Console.WriteLine("введите данные правильно");
+}

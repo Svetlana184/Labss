de72aa9df0772eaafbe23465f72e6af49255f9ea
Svetlana184 <sveta.kgd.39@mail.com>
2024-10-16 23:24:00 +0200

    try

---
+++ LAB_2.3._with_functions/Program.cs
@@ -0,0 +1,30 @@
+﻿try
+{
+    Console.WriteLine("Введите x:");
+    double x = double.Parse(Console.ReadLine());
+    static double func_1(double x, double a, double b, double z)
+    {
+        double y;
+        if (x <= 5 *a) 
+        { 
+            y = 2.5 * b * b + a * x - 4.5 * Math.Cos(x * z); 
+        }
+        else if ( x > b)
+        { 
+            y = Math.Pow(a * a - 5.4 * x, 3) + Math.Log(x * z); 
+        }
+        else 
+        { 
+            y = Math.Sqrt(6.5 * b * b + (a - Math.Pow(x, 3) * z)); 
+        }
+        return y;
+    }
+    Console.WriteLine($"y = {(func_1(x, 0.5, 4.5, Math.Pow(Math.E, 0.5 * x))):N2}");
+    Console.WriteLine($"y = {(func_1(x, 0.5, 3.7, Math.Pow(Math.E, 2 * 0.5 * x))):N2}");
+    Console.WriteLine($"y = {(func_1(x, 0.5, 2.7, Math.Pow(Math.E, 2.5 * 0.5 * x))):N2}");
+}
+
+catch
+{
+    Console.WriteLine("введите данные правильно");
+}
---
+++ LAB_2.3/Program.cs
@@ -0,0 +1,46 @@
+﻿try
+{
+    Console.WriteLine("Введите x:");
+    double x = double.Parse(Console.ReadLine());
+    double y1, y2, y3, a = 0.5, b1 = 4.5, b2 = 3.7, b3 = 2.7;
+    double z1 = Math.Pow(Math.E, a * x), z2 = Math.Pow(Math.E, 2 * a * x), z3 = Math.Pow(Math.E, 2.5 * a * x);
+    if (x <= 5 * a)
+    {
+        y1 = 2.5 * b1 * b1 + a * x - 4.5 * Math.Cos(x * z1);
+        y2 = 2.5 * b2 * b2 + a * x - 4.5 * Math.Cos(x * z2);
+        y3 = 2.5 * b3 * b3 + a * x - 4.5 * Math.Cos(x * z3);
+    }
+    else if (x > b1)
+    {
+        y1 = Math.Pow(a * a - 5.4 * x, 3) + Math.Log(x * z1);
+        y2 = Math.Pow(a * a - 5.4 * x, 3) + Math.Log(x * z2);
+        y3 = Math.Pow(a * a - 5.4 * x, 3) + Math.Log(x * z3);
+    }
+    else
+    {
+        y1 = Math.Sqrt(6.5 * b1 * b1 + (a - Math.Pow(x, 3) * z1));
+        if (x > b2)
+        {
+            y2 = Math.Pow(a * a - 5.4 * x, 3) + Math.Log(x * z2);
+            y3 = Math.Pow(a * a - 5.4 * x, 3) + Math.Log(x * z3);
+        }
+        else
+        {
+            y2 = Math.Sqrt(6.5 * b2 * b2 + (a - Math.Pow(x, 3) * z2));
+            if (x > b3)
+            {
+                y3 = Math.Pow(a * a - 5.4 * x, 3) + Math.Log(x * z3);
+            }
+            else
+            {
+                y3 = Math.Sqrt(6.5 * b3 * b3 + (a - Math.Pow(x, 3) * z3));
+            }
+        }
+    }
+    Console.WriteLine($"y(x)={y1:N2}\ny(x)={y2:N2}\ny(x)={y3:N2}");
+}
+
+catch
+{
+    Console.WriteLine("введите данные правильно");
+}

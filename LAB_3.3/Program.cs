41ca053aa2e772c5baeb5057f2db6b53aa28ad51
Svetlana184 <sveta.kgd.39@gmail.com>
2024-10-25 17:23:50 +0200

    try

---
+++ LAB_3.3/Program.cs
@@ -0,0 +1,22 @@
+﻿try
+{
+    Console.WriteLine("введите x:");
+    int x = int.Parse(Console.ReadLine());
+    Console.WriteLine("введите n:");
+    int n = int.Parse(Console.ReadLine());
+    double s = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        long factorial = 1;
+        for (int j = 1; j <= i; j++)
+        {
+            factorial *= j;
+        }
+        s += (Math.Acos(i * (Math.PI / 4))) * Math.Pow(x, i) / factorial;
+    }
+    Console.WriteLine($"S = {s:N2}");
+}
+catch
+{
+    Console.WriteLine("введите число правильно!");
+}

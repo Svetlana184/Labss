879de5918dafc736db510e9a9418f09bb5aec5f0
Svetlana184 <sveta.kgd.39@mail.com>
2024-10-26 17:45:18 +0200

    try

---
+++ LAB_3.4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("┌---------------------------------------┐");
+Console.WriteLine($"|         x         |        f(x)       |");
+Console.WriteLine("|---------------------------------------|");
+for (double x = -3; x <= 2.5; x += 1.1)
+{
+    double y = 0;
+    if (x > 0)
+    {
+        
+        for (int k = 1; k <= 8; k++) 
+        {
+            long factorial = 1;
+            for (int j = 1; j <= k; j++) factorial *= j;
+            y += Math.Pow(-1, k)* Math.Pow(x, k) / factorial; 
+        }
+    }
+    else y = Math.Tan(Math.Pow(Math.PI, x));
+    Console.WriteLine($"|       {x,5:F2}       |       {y,5:F2}       |");
+}
+Console.WriteLine("└---------------------------------------┘");
+Console.WriteLine();

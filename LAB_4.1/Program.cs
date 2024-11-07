1f4015a747e15c42e25d739c4a8f918512b95d20
Svetlana184 <sveta.kgd.39@gmail.com>
2024-11-07 12:43:38 +0200

    t

--- LAB_4.1/Program.cs
+++ LAB_4.1/Program.cs
@@ -36,31 +36,3 @@ for (int i = 0;i < mas.Length; i++)
 double[] res = Array.FindAll(mas, p => p != 0);
 foreach (int item in res) Console.Write(item + " ");
 Console.WriteLine();
-
-//double[] mas = new double[12];
-//Random random = new Random();
-//for (int i = 0;i  < mas.Length;i++)
-//{
-//    mas[i] = random.NextDouble() * 90 + 10;
-//    Console.Write($"{mas[i]:F2} ");
-//}
-//double min = double.MaxValue;
-//int imin = -1;
-//for  (int i = 0; i < mas.Length;i++)
-//{
-//    if (mas[i] < min)
-//    {
-//        min = mas[i]; imin = i;
-//    }
-//}
-//Console.WriteLine($"min = {(min+imin):F2}");
-
-////средний уровень
-//Random random1 = new Random();
-//int length = random1.Next(10, 20);
-//double[] mas1  = new double[length];
-//double[] mas2 = new double[length];
-//for (int i = 0; i  < length;i++)
-//{
-
-//}

5182e4f24262b74274f58c5d792dcd0e9e74116b
Svetlana184 <sveta.kgd.39@mail.com>
2024-11-08 12:05:48 +0200

    t

--- LAB_4.1/Program.cs
+++ LAB_4.1/Program.cs
@@ -1,29 +1,29 @@
 ﻿double[] mas = new double[12];
 Random random = new Random();
-// рандомный массив с двоичными числами
 for (int i = 0; i < mas.Length; i++)
 {
     int temp = random.Next(10, 100);
-    Console.WriteLine(temp);
     int counter = 0;
     mas[i] = 0;
     while (temp > 1)
     {
-        mas[i] = mas[i] + (temp%2) * Math.Pow(10, counter);
-        counter+=1;
+        mas[i] = mas[i] + (temp % 2) * Math.Pow(10, counter);
+        counter += 1;
         temp /= 2;
     }
-    mas[i] += 1* Math.Pow(10, counter);
-    Console.WriteLine(mas[i]);
+    mas[i] += 1 * Math.Pow(10, counter);
 
 }
-for (int i = 0;i < mas.Length; i++)
+Console.WriteLine("необработанный массив:");
+foreach (int item in mas) Console.Write(item + " ");
+Console.WriteLine();
+for (int i = 0; i < mas.Length; i++)
 {
     double ex = mas[i];
     int counter2 = 0;
     for (int j = i; j < mas.Length; j++)
     {
-        if(ex == mas[j])
+        if (ex == mas[j])
         {
             counter2++;
             if (counter2 > 2)
@@ -34,5 +34,6 @@ for (int i = 0;i < mas.Length; i++)
     }
 }
 double[] res = Array.FindAll(mas, p => p != 0);
+Console.WriteLine("обработанный массив:");
 foreach (int item in res) Console.Write(item + " ");
 Console.WriteLine();
